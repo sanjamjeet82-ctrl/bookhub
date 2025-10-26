8096632ecd4e3fb893704bb02884e9d9606c8b13
sanjamjeet82-ctrl <Sanjamjeet82@gmail.com>
2025-10-26 17:01:50 +0530

    git commit -m "first commit"

---
+++ movie_booking/build.sh
@@ -0,0 +1,18 @@
+#exit on error
+
+set -o errexit
+
+netfis
+
+pip install -r requirements.txt
+
+python manage.py collectstatic  --no-input
+
+python manage.py migrate
+
+if [[ $CREATE_SUPERUSER  ]];
+
+then
+
+  python manage.py createsuperuser --no-input-email "$DJANGO_SUPERUSER_EMAIL"
+fi
